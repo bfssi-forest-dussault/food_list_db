432ca7775806ac34a3e79f6ae790699b2fb54be0
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-09-10 16:16:40 -0400

    register models

--- food_list_db/database/admin.py
+++ food_list_db/database/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from food_list_db.database import models
 
 # Register your models here.
+admin.site.register(models.FoodCoding)
+admin.site.register(models.BNSFoodCoding)
+admin.site.register(models.Category)
+admin.site.register(models.FoodItem)
