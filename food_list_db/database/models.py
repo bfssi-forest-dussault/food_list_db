76d2d56a9948f41cf67f5a1c6612c2726bd0b8b7
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-09-10 16:17:10 -0400

    add max_size to charfield, __str__ methods, Meta

--- food_list_db/database/models.py
+++ food_list_db/database/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
+# Sensible field sizes for CharField columns
+LG_CHAR = 1500
+MD_CHAR = 500
+SM_CHAR = 50
+
 
 class Category(models.Model):
     """
     Support table
     """
-    category = models.CharField(unique=True)
-    description = models.CharField(null=True, blank=True)
+    category = models.CharField(unique=True, max_length=SM_CHAR)
+    description = models.CharField(null=True, blank=True, max_length=MD_CHAR)
+
+    def __str__(self):
+        return f"{self.category}"
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
 
 class FoodCoding(models.Model):
@@ -14,15 +26,29 @@ class FoodCoding(models.Model):
     Support table
     """
     fid_cde = models.IntegerField(unique=True)  # Food code
-    fdc_den = models.CharField()  # Food description english
+    fdc_den = models.CharField(max_length=MD_CHAR)  # Food description english
+
+    def __str__(self):
+        return f"{self.fid_cde}: {self.fdc_den}"
+
+    class Meta:
+        verbose_name = 'Food Coding'
+        verbose_name_plural = 'Food Coding'
 
 
 class BNSFoodCoding(models.Model):
     """
     Support table
     """
-    fid_fgr = models.CharField(unique=True)  # BNS food code
-    fdc_fge = models.CharField()  # BNS food group description
+    fid_fgr = models.CharField(unique=True, max_length=SM_CHAR)  # BNS food code
+    fdc_fge = models.CharField(max_length=MD_CHAR)  # BNS food group description
+
+    def __str__(self):
+        return f"{self.fid_fgr}: {self.fdc_fge}"
+
+    class Meta:
+        verbose_name = 'BNS Food Coding'
+        verbose_name_plural = 'BNS Food Coding'
 
 
 class FoodItem(models.Model):
@@ -33,3 +59,10 @@ class FoodItem(models.Model):
     bns_category = models.ForeignKey(BNSFoodCoding, on_delete=models.CASCADE, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     comments = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.food}"
+
+    class Meta:
+        verbose_name = 'Food Item'
+        verbose_name_plural = 'Food Items'
